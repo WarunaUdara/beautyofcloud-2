3adf8b306bd60d655db6c647f4dddef7088574c0
Senidu Ravihara <senidumee16@gmail.com>
2026-04-12 00:43:37 +0530

    update firebase configs

--- boc-web/src/firebase/config.ts
+++ boc-web/src/firebase/config.ts
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAdyGTbvIruNH_vjwSo4N90fFVqtT0vCk4",
+  authDomain: "boc-web-a1a9f.firebaseapp.com",
+  projectId: "boc-web-a1a9f",
+  storageBucket: "boc-web-a1a9f.firebasestorage.app",
+  messagingSenderId: "705127827058",
+  appId: "1:705127827058:web:561a78bf06fdf929b0a4eb"
 };
 
 // Initialize Firebase
